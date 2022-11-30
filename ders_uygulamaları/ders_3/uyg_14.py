546b1affc140cb24976482bd1b9b0d5844b10f93
Abdullah Köse <fb_1907apo@hotmail.com>
2022-12-01 00:16:43 +0300

    1.12

--- ders_uygulamaları/ders_3/uyg_14.py
+++ ders_uygulamaları/ders_3/uyg_14.py
@@ -1,24 +1,38 @@
 
 
 """
-Modüller(Kütüphane) 2'ye ayrılır 
+                MODÜL (KÜTÜPHANE)
+
+Modüller(Kütüphane) 2'ye ayrılır
 
 1-Kendi Yazdığımız Modüller
 2-Hazır Modüller
+--------------------------------------------------------------------------------------------------------------------------------------
+1-KENDİ YAZDIĞIMIZ MODÜLLER
 
-"""
+Bu işlemi kullanmak için iki python kodu olması gerekir. 
 
-"""
------------- KENDİ YAZGIĞIMIZ MODÜLLER----------------
+-Birinci kod modül kodu. Bu kodda kullanılması istenilen işlemler fonksiyon olarak tanımlanır. 
 
---- Bu işlemi kullanmak için iki python kodu olması gerekir. 
-- Birinci kod modül kodu. Bu kodda kullanılması istenilen işlemler fonksiyon olarak tanımlanır. 
-- İkinci kod ise yazdğımız kodların olduğu bir python kodudur. Bu python kodunda ise diğer kodda hazırlağımız modül içe aktarılır(import) ve sonra orda yazmış olduğumuz fonksiyonlardan istediğimizi çağırıp bu kodda kullanabiliriz.
+-İkinci kod ise yazdığımız kodların olduğu bir python kodudur. Bu python kodunda ise diğer kodda hazırladığımız modül içe aktarılır(import) ve sonra orda yazmış olduğumuz fonksiyonlardan istediğimizi çağırıp bu kodda kullanabiliriz.
 
 !!! bu işlemde dikkat edilmesi gereken konu ise modül kodu iki yerde bulanabilir.
-!!! birinci yer: python kodunu yazdığımız klasörle aynı klasörün içinde
-!!! ikinci yer: python kurulumunu yaptığımız yerin altında bulunan Lib klasörü içinde (eğer bu konuma kaydedersek her kodda bu modülü kullanabiliriz)
+            !! birinci yer: python kodunu yazdığımız klasörle aynı klasörün içinde
+            !! ikinci yer: python kurulumunu yaptığımız yerin altında bulunan Lib klasörü içinde (eğer bu konuma kaydedersek her kodda bu modülü kullanabiliriz)
+--------------------------------------------------------------------------------------------------------------------------------------
+2- HAZIR MODÜLLER
 
+--- Hazır Modüller python’da iki grupta incelenir
 
+- Birinci Grup:
+    Pythonı bilgisayarımıza kurduğumuzda direk olarak kurulan modüller (örn; random , math, os , sys vb.)
+    !!! Bu modülleri kullanmak için sadece kodumuza "importh modül_adı" ile dahil etmemiz gerekir
 
+- İkinci Grup:
+    İnternetten başka kişlerin yazdığı modülleri indirip kullanabiliriz.
+    !!! Bu yöntemi kullanmak için gerekli olan modül belirlendikten sonra terminal ekranına (diğer adı komut satırı veya cmd ekranına) şu komutla direk olarak indirip kullanabiliriz. Komut adı ise şöyledir "pip install modül_adı" olarak indirip kullanabiliriz
 """
+
+
+
+
