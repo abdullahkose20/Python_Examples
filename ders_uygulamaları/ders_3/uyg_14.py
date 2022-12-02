da0e06e7d5951e7b611f58f2100bf95555a24a97
Abdullah Köse <fb_1907apo@hotmail.com>
2022-12-03 01:18:50 +0300

    3.12

--- ders_uygulamaları/ders_3/uyg_14.py
+++ ders_uygulamaları/ders_3/uyg_14.py
@@ -34,5 +34,234 @@ Bu işlemi kullanmak için iki python kodu olması gerekir.
 """
 
 
+"""
+örnek-1
+-------modül kodu-------(nct.py)
+def ornek():
+    print("Merhaba Abdullah")   
+    
+------python kodu-------
+
+import nct
+
+nct.ornek()
+
+"""
 
 
+
+
+"""
+örnek-2
+-------modül kodu-------(hesaplama.py)
+def toplam():
+    print(11+3)
+
+def fark():
+    print(11-3)  
+
+------python kodu-------
+
+import hesaplama
+
+hesaplama.toplam()
+print("merhaba")
+hesaplama.fark()
+
+"""
+
+"""
+örnek-3
+-------modül kodu-------(param.py)
+def toplam(a,b):
+    print(a+b)
+
+def fark(a,b):
+    print(a-b)  
+
+------python kodu-------
+
+import param
+
+param.toplam(15,5)
+param.fark(28,3)
+
+"""
+
+
+"""
+örnek-4
+
+-------modül kodu-------(okul.py)
+
+def kayıt(isim, soyisim, işsis, şehir):
+    print("-"*30)   
+    print("isim           : ", isim)
+    print("soyisim        : ", soyisim)
+    print("telefon        : ", işsis)
+    print("şehir          : ", şehir)   
+    print("-"*30)
+
+------python kodu-------
+
+import okul
+
+okul.kayıt("abdullah","köse","05414428683","denizli")
+
+okul.kayıt("yaman","ebecek","05556667788","denizli")
+
+"""
+
+
+
+"""
+örnek-5
+
+-------modül kodu-------(hesap.py)
+
+def toplam(sayı_1,sayı_2):
+    print("toplam:", sayı_1+sayı_2)
+
+def fark(sayı_1,sayı_2):
+    print("fark:", sayı_1-sayı_2)
+
+def çarpım(sayı_1,sayı_2):
+    print("çarpım:", sayı_1*sayı_2)
+
+def bölüm(sayı_1,sayı_2):
+    print("bölüm:", sayı_1/sayı_2)
+
+
+
+------python kodu-------
+
+import hesap
+
+sayı_1=int(input("birinci Sayı giriniz"))
+sayı_2=int(input("ikinci Sayı giriniz"))
+islem=input("işlem seçiniz")
+
+if islem=="+":
+    hesap.toplam(sayı_1,sayıı_2)
+if islem=="-":
+    hesap.fark(sayı_1,sayıı_2)
+if islem=="*":
+    hesap.çarpım(sayı_1,sayıı_2)
+if islem=="/":
+    hesap.bölüm(sayı_1,sayıı_2)
+else:
+    print("yanlış işlem girdiniz")
+
+"""
+
+
+"""
+
+****************************************
+            HAZIR MODÜLLER
+****************************************
+"""
+# -----------------TİME--------------------------------------------
+
+"""
+# örnek 1 //zamanı gösterme
+
+import time
+
+print(time.localtime()) 
+"""
+
+"""
+# örnek 2 //zamanı düzenli gösterme
+
+import time
+
+print(time.asctime())
+"""
+
+"""
+# örnek 3 // bekleme
+
+import time
+
+print("abdullah")
+
+time.sleep(3)
+
+print("köse")
+
+"""
+# -----------------RANDOM--------------------------------------------
+"""
+# örnek 1 //rasgele sayı seçme
+
+import random
+
+print(random.randint(0,50))
+
+"""
+
+"""
+# örnek 2 //listeden rastgele sayı üretme
+
+import random
+
+nct=["ayşe","ali","veli","45","12"]
+
+print(random.choice(nct))
+
+"""
+
+"""
+# örnek 3 //listeyi karışık sıralama
+
+import random
+
+sayılar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10]
+
+random.shuffle(sayılar)
+
+print(sayılar)
+
+"""
+
+
+# -----------------MATH--------------------------------------------
+"""
+# örnek 1 //sayıyı tam sayıya tamamlama 
+
+ **math.ceil()  üst sayıya tamamlar
+ **math.floor() alt sayıya tamamlar 
+
+import math
+
+k=35.2
+
+y=74.8
+
+print(math.ceil(k))
+
+print(math.floor(y))
+"""
+
+"""
+# örnek 2 //faktoriyel hesabı
+
+import math
+
+f = 4
+
+print(math.factorial(f))
+
+"""
+
+"""
+# örnek 3 //üst hesaplama , karekök bulma ,
+
+import math
+
+print(math.pow(5,2))
+
+print(math.sqrt(81))
+
+"""
---
+++ ders_uygulamaları/ders_3/uyg_15.py
@@ -0,0 +1,19 @@
+import time
+import random
+import math
+
+import os
+import sys
+import subprocess
+import webbrowser
+import turtle
+import smtplib
+
+
+
+
+
+
+
+
+
